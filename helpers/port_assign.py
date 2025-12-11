label rotation and rendering correct

helpers/port_assign.py
@@ -5,14 +5,30 @@ from elements.network import *
 
 ### GEOGRAPHIC COST CALCULATIONS ###
 
-from numpy import matrix
+import numpy as np
+
 def angle_error( a, b ):
     diff = abs(a-b) % (2*pi)
     return min(diff, 2*pi-diff)
-def cost_matrix( v ):
+
+def cost_matrix( v: Node):
     port_angles = [ i*(pi/4) for i in range(8) ]
     edge_angles = [ e.geo_angle(v) for e in v.edges ]
-    return matrix( [ [ angle_error(pa,ea)**2 for pa in port_angles ] for ea in edge_angles ] )
+    return np.matrix( [ [ angle_error(pa,ea)**2 for pa in port_angles ] for ea in edge_angles ] )
+
+# Two things to check for: 
+# - We give priority to labels that are horizontal so 0 and 4 and don't want 2 and 6 and for the odd numbers they should be equal
+# - We want labels that are on the outside to appear on the outside (either do this by a weighted middle point of the network or check whether labels point towards the outer face instead of an inner face)
+def cost_matrix_labels(v: Node, label_strength: float, mid_point_x): 
+    port_angles = [ i*(pi/4) for i in range(8) ]
+    edge_angles = [ e.geo_angle(v) for e in v.edges ]
+    port_edge_matrix = np.matrix( [ [ angle_error(pa,ea)**2 for pa in port_angles ] for ea in edge_angles ] )
+    wl = [0.01 * label_strength, 0.02 * label_strength, 0.03 * label_strength]
+
+    if v.geo_pos.x() <= mid_point_x: 
+        return np.vstack([port_edge_matrix, np.array([wl[0] / 2, wl[1] /2 , wl[2], wl[1], wl[0], wl[1], wl[2], wl[1] / 2])])
+    else: 
+        return np.vstack([port_edge_matrix, np.array([wl[0], wl[1], wl[2], wl[1] / 2, wl[0] / 2, wl[1] /2 , wl[2], wl[1]])])
 
 ### ROUNDING ###
 
@@ -28,15 +44,21 @@ def assign_by_rounding( net ):
 ### MATCHING ###
 
 from scipy.optimize import linear_sum_assignment
-def assign_by_local_matching( net: Network ):
+def assign_by_local_matching( net: Network, label_strength: float ):
+    print("STARTTTT")
+    net.evict_all_labels()
     net.evict_all_edges()
     for v in net.nodes.values():
-        costs = cost_matrix(v)
+        print(v.ports)
+    for v in net.nodes.values():
+        costs = cost_matrix_labels(v, label_strength, net.midpoint.x())
         _, cols = linear_sum_assignment(costs)
-        for i,p in enumerate(cols):
+        print(cols)
+        for i,p in enumerate(cols[:-1]):
             v.assign( v.edges[int(i)], int(p) )
-        v.label_node.port = v.first_free_port()
-        v.ports[v.label_node.port] = v.label_node
+        v.assign_label(int(cols[-1]))
+        # v.label_node.port = v.first_free_port()
+        # v.ports[v.label_node.port] = v.label_node
 
 
 ### INTEGER LINEAR PROGRAMMING ###